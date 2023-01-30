c41bd9379def83f5071bf468455eef2db8b8c8af
Mohamed <mohamed.eltanani@vantaca.com>
2023-01-29 23:26:55 -0500

    added sepration for scaffold

--- app/src/main/java/com/example/composedestinations/MainActivity.kt
+++ app/src/main/java/com/example/composedestinations/MainActivity.kt
@@ -4,146 +4,133 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.text.BasicText
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.FormatListBulleted
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.example.composedestinations.destinations.SecondScreenDestination
-import com.example.composedestinations.ui.theme.ComposeDestinationsTheme
+import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.composedestinations.destinations.ChatScreenDestination
+import com.example.composedestinations.destinations.HomeScreenDestination
 import com.ramcosta.composedestinations.DestinationsNavHost
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootNavGraph
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
-import com.ramcosta.composedestinations.navigation.EmptyDestinationsNavigator
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
 
-           DestinationsNavHost(navGraph = NavGraphs.root)
-
+            DestinationsNavHost(navGraph = NavGraphs.root)
 
         }
     }
 }
 
-
-@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @RootNavGraph(start = true)
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Destination
 @Composable
-fun FirstScreen(navigator: DestinationsNavigator) {
+fun MyScaffold(navigator: DestinationsNavigator) {
 
-    BasicText(text = "wowowo")
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(color = Color.LightGray),
-
-        ) {
-        BasicText(
-            style = TextStyle(color = Color.Red, fontSize = 42.sp),
-            modifier = Modifier.offset(y = 140.dp),
-            text = "Use bottom navigation bar to navigate to second screen and you will see it disappear"
-        )
-    }
-    val items = listOf(
-        BottomNavItem(
-            name = "Home",
-            route = "home",
-            icon = Icons.Default.Home
-        ),
-        BottomNavItem(
-            name = "Associations",
-            route = "associations",
-            icon = Icons.Default.FormatListBulleted
-        )
-    )
+    val navController = rememberNavController()
 
     Scaffold(
         bottomBar = {
-
-            //val backStackEntry = navController.currentBackStackEntryAsState()
-            BottomNavigation(
-                backgroundColor = Color.Green,
-                contentColor = Color.Red
-            ) {
-
-                items.forEach { item ->
-                    //     val selected = item.route == backStackEntry.value?.destination?.route
-
-                    BottomNavigationItem(
-                        selected = true,
-                        onClick = {
-                            //navController.navigate(item.route)
-                            navigator.navigate(SecondScreenDestination)
-                        },
-                        selectedContentColor = Color.Blue,
-                        unselectedContentColor = Color.Green,
-                        icon = {
-                            Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                                Icon(
-                                    imageVector = item.icon,
-                                    contentDescription = item.name
-                                )
-                                Text(text = item.name)
-                            }
-                        }
-                    )
+            BottomNavigationBar(
+                items = listOf(
+                    BottomNavItem(
+                        name = "Home",
+                        route = "home",
+                        icon = Icons.Default.Home
+                    ),
+                    BottomNavItem(
+                        name = "Chat",
+                        route = "chat",
+                        icon = Icons.Default.Notifications,
+                        badgeCount = 23
+                    ),
+                ),
+                navController = navController,
+                onItemClick = {
+                    if (it.name == "Chat")
+                        navigator.navigate(ChatScreenDestination)
+                    else if (it.name == "Home")
+                        navigator.navigate(HomeScreenDestination)
                 }
-            }
+            )
         }
     ) {
     }
+}
 
+
+@Destination
+@Composable
+fun BottomNavigationBar(
+    items: List<BottomNavItem>,
+    navController: NavController,
+    modifier: Modifier = Modifier,
+    onItemClick: (BottomNavItem) -> Unit
+) {
+    val backStackEntry = navController.currentBackStackEntryAsState()
+    BottomNavigation(
+        modifier = modifier,
+        backgroundColor = Color.DarkGray,
+        elevation = 5.dp
+    ) {
+        items.forEach { item ->
+            val selected = item.route == backStackEntry.value?.destination?.route
+            BottomNavigationItem(
+                selected = selected,
+                onClick = { onItemClick(item) },
+                unselectedContentColor = Color.Gray,
+                icon = {
+                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+
+                        Icon(
+                            imageVector = item.icon,
+                            contentDescription = item.name
+                        )
+                    }
+                }
+            )
+        }
+    }
 }
 
 @Destination
 @Composable
-fun SecondScreen(navigator: DestinationsNavigator) {
-
+fun HomeScreen() {
     Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(color = Color.Black),
-
-        ) {
-        BasicText(
-            style = TextStyle(color = Color.Green, fontSize = 42.sp),
-            modifier = Modifier.offset(y = 140.dp),
-            text = "Second Screen and we should see scaffold, but is gone when I navigate use compose destination"
-        )
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Home screen")
     }
-
 }
 
-
-data class BottomNavItem(
-    val name: String,
-    val route: String,
-    val icon: ImageVector
-)
-
-@Preview(showBackground = true)
+@Destination
 @Composable
-fun DefaultPreview() {
+fun ChatScreen() {
 
-        FirstScreen(EmptyDestinationsNavigator)
-
+    //This could work but I don't think because what destinationNavigator to send
+    //MyScaffold(navigator = ??)
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Chat screen")
+    }
 }
